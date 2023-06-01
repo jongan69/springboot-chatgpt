ff14762f6ab01f118685d1208d365258097eb5f4
Jongan69 <jonathang132298@gmail.com>
2023-05-31 21:43:17 -0400

    First Commit

---
+++ src/test/java/com/chatgpt/ChatgptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
